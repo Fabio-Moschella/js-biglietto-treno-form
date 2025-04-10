bc5528ea62f425f9f3e76c7faff761915d55ca5f
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-10 16:03:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const nameAndSurnameInput = document.querySelector("#nameinput");
+const kmtocover = document.querySelector("#kminput");
+const generateButtonEl = document.querySelector("#generatebutton");
+
+// // Ottengo dall'utente la sua età e li converto in intero
+// const etautente = parseInt(prompt("Quanti anni hai?"));
+
+// // Ottengo dall'utente i km che deve percorrere e li converto in intero
+// const kmPercorsi = parseInt(prompt("Quanti chilometri devi percorrere?"));
+
+// const prezzoKm = kmPercorsi * 0.21;
+// const scontoDel20 = (prezzoKm * 20) / 100;
+// const scontoDel40 = (prezzoKm * 40) / 100;
+// const prezzoUnder18 = prezzoKm - scontoDel20;
+// const prezzoOver65 = prezzoKm - scontoDel40;
+// let outputText = "Non puoi usare lettere, solo numeri";
+
+// console.log(etautente, "etautente");
+// console.log(prezzoKm, "prezzokm");
+// console.log(prezzoUnder18, "prezzounder18");
+// console.log(prezzoOver65, "prezzoover65");
+
+// // Se l'utente è < 18
+// if (etautente < 18) {
+//   //   Stampa il prezzo per gli under18
+//   console.log(prezzoUnder18, "prezzounder18");
+//   // Se invece l'utente è > 65
+// } else if (etautente > 65) {
+//   //   Stampa il prezzo per gli over65
+//   console.log(prezzoOver65, "prezzoover65");
+//   // Altrimenti
+// } else {
+//   //   Stampa il prezzo standard
+//   console.log(prezzoKm, "prezzostandard");
+//}
